48f9a8e746ed68a4a50b4a654808e1b07cbf712e
Aya <aae2042@nyu.edu>
2024-10-10 00:22:27 +0200

    2 commits: Githubactions and creating the people's file with the people dictionaries

---
+++ data/people.py
@@ -0,0 +1,28 @@
+"""
+module will interface to the user's data 
+"""
+
+MIN_USER_NAME_LEN = 2
+# fields
+NAME = 'name'
+ROLES = 'roles'
+AFFILIATION = 'affiliation'
+EMAIL = 'email'
+
+TEST_EMAIL = 'aae2042@nyu.edu'
+DEL_EMAIL = 'delete@nyu.edu'
+
+people_dict = {
+    TEST_EMAIL: {
+        NAME: 'Aya Elfettahi',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: TEST_EMAIL,
+    },
+    DEL_EMAIL: {
+        NAME: 'Stranger',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: DEL_EMAIL,
+    },
+}
