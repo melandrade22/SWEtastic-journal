f3bdcf14eed88a5764767c06a495414aeeaf2b3c
Melanie <ma6700@nyu.edu>
2024-10-17 10:43:39 -0400

    adding test_people with tests appended

---
+++ data/tests/test_people.py
@@ -0,0 +1,37 @@
+import pytest
+
+import data.people as ppl
+
+ADD_EMAIL = 'person@nyu.edu'
+
+def test_read():
+    people = ppl.read()
+    assert isinstance(people, dict)
+    assert len(people) > 0
+    # check for string IDs:
+    for _id, person in people.items():
+        assert isinstance(_id, str)
+        assert ppl.NAME in person
+
+
+# def test_delete():
+#     people = ppl.read()
+#     old_len = len(people)
+#     ppl.delete(ppl.DEL_EMAIL)
+#     people = ppl.read()
+#     assert len(people) < old_len
+#     assert ppl.DEL_EMAIL not in people
+
+
+def test_create():
+    people = ppl.read()
+    assert ADD_EMAIL not in people
+    ppl.create('Bob', 'NYU', ADD_EMAIL)
+    people = ppl.read()
+    assert ADD_EMAIL in people
+
+
+def test_create_duplicate():
+    with pytest.raises(ValueError):
+        ppl.create('Do not care about name',
+                   'Or affiliation', ppl.TEST_EMAIL)
