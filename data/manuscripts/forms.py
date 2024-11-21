5d7f274edd7d54af831eddc6634ff549939aace5
Aya <aae2042@nyu.edu>
2024-11-21 09:47:16 +0100

    added manuscript folder and created the forms.py and manuscript_req.md file

---
+++ data/manuscripts/forms.py
@@ -0,0 +1,37 @@
+"This module will provide a sample query form"
+
+import backendcore.data.form_filler as ff
+
+from templates.fields import CODE 
+
+FORM_FLDS = [{
+    ff.FLD_NM: CODE,
+    ff.QSTN: 'Sample:',
+    ff.PARAM_TYPE: ff.QUERY_STR,
+},
+]
+
+
+def get_form() -> list:
+    return FORM_FLDS
+
+
+def get_form_descr():
+    """
+    For Swagger!
+    """
+    return ff.get_form_descr(FORM_FLDS)
+
+
+def get_fld_names() -> list:
+    return ff.get_fld_names(FORM_FLDS)
+
+
+def main():
+    printf(f'Form: {get_form()=}')
+    printf(f'Form: {get_form_descr()=}')
+    printf(f'Field names: {get_fld_names()=}')
+
+
+if __name__ == "__main__":
+    main()
