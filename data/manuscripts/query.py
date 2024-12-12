43b64e60b5df74c4e2d31e30d76adc4714bee2e3
Melanie <ma6700@nyu.edu>
2024-12-12 16:11:40 -0500

    update query.py and test to demo version

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -1,16 +1,29 @@
+import data.manuscripts.fields as flds
+
 # states:
+AUTHOR_REV = 'AUR'
 COPY_EDIT = 'CED'
 IN_REF_REV = 'REV'
 REJECTED = 'REJ'
 SUBMITTED = 'SUB'
+WITHDRAWN = 'WIT'
 TEST_STATE = SUBMITTED
 
 VALID_STATES = [
+    AUTHOR_REV,
     COPY_EDIT,
     IN_REF_REV,
     REJECTED,
     SUBMITTED,
+    WITHDRAWN,
 ]
+
+
+SAMPLE_MANU = {
+    flds.TITLE: 'Sample Manuscript',
+    flds.AUTHOR: 'Author',
+    flds.REFEREES: [],
+}
 
 
 def get_states() -> list:
@@ -24,16 +37,20 @@ def is_valid_state(state: str) -> bool:
 # actions:
 ACCEPT = 'ACC'
 ASSIGN_REF = 'ARF'
+DELETE_REF = 'DRF'
 DONE = 'DON'
 REJECT = 'REJ'
+WITHDRAW = 'WIT'
 # for testing:
 TEST_ACTION = ACCEPT
 
 VALID_ACTIONS = [
     ACCEPT,
     ASSIGN_REF,
+    DELETE_REF,
     DONE,
     REJECT,
+    WITHDRAW,
 ]
 
 
@@ -45,18 +62,65 @@ def is_valid_action(action: str) -> bool:
     return action in VALID_ACTIONS
 
 
+def assign_ref(manu: dict, ref: str, extra=None) -> str:
+    print(extra)
+    manu[flds.REFEREES].append(ref)
+    return IN_REF_REV
+
+
+def delete_ref(manu: dict, ref: str) -> str:
+    if len(manu[flds.REFEREES]) > 0:
+        manu[flds.REFEREES].remove(ref)
+    if len(manu[flds.REFEREES]) > 0:
+        return IN_REF_REV
+    else:
+        return SUBMITTED
+
+
 FUNC = 'f'
+
+COMMON_ACTIONS = {
+    WITHDRAW: {
+        FUNC: lambda **kwargs: WITHDRAWN,
+    },
+}
 
 STATE_TABLE = {
     SUBMITTED: {
-        ASSIGN_REF:{
-            FUNC: lambda x: IN_REF_REV,
+        ASSIGN_REF: {
+            FUNC: assign_ref,
         },
         REJECT: {
-            FUNC: lambda x: REJECTED,
+            FUNC: lambda **kwargs: REJECTED,
         },
-    }
+        **COMMON_ACTIONS,
+    },
+    IN_REF_REV: {
+        ASSIGN_REF: {
+            FUNC: assign_ref,
+        },
+        DELETE_REF: {
+            FUNC: delete_ref,
+        },
+        **COMMON_ACTIONS,
+    },
+    COPY_EDIT: {
+        DONE: {
+            FUNC: lambda **kwargs: AUTHOR_REV,
+        },
+        **COMMON_ACTIONS,
+    },
+    AUTHOR_REV: {
+        **COMMON_ACTIONS,
+    },
+    REJECTED: {
+        **COMMON_ACTIONS,
+    },
+    WITHDRAWN: {
+        **COMMON_ACTIONS,
+    },
 }
+
 
 
 def get_valid_actions_by_state(state: str):
@@ -65,28 +129,25 @@ def get_valid_actions_by_state(state: str):
     return valid_actions
 
 
-def handle_action(curr_state, action) -> str:
-    if not is_valid_state(curr_state):
-        raise ValueError(f'Invalid state: {curr_state}')
-    if not is_valid_action(action):
-        raise ValueError(f'Invalid action: {action}')
-    new_state = curr_state
-    if curr_state == SUBMITTED:
-        if action == ASSIGN_REF:
-            new_state = IN_REF_REV
-        elif action == REJECT:
-            new_state = REJECTED
-    elif curr_state == IN_REF_REV:
-        if action == ACCEPT:
-            new_state = COPY_EDIT
-        elif action == REJECT:
-            new_state = REJECTED
-    return new_state
+def handle_action(curr_state, action, **kwargs) -> str:
+    if curr_state not in STATE_TABLE:
+        raise ValueError(f'Bad state: {curr_state}')
+    if action not in STATE_TABLE[curr_state]:
+        raise ValueError(f'{action} not available in {curr_state}')
+    return STATE_TABLE[curr_state][action][FUNC](**kwargs)
 
 
 def main():
-    print(handle_action(SUBMITTED,ASSIGN_REF))
-    print(handle_action(SUBMITTED,REJECT))
+    print(handle_action(SUBMITTED, ASSIGN_REF,
+                        manu=SAMPLE_MANU, ref='Jack'))
+    print(handle_action(IN_REF_REV, ASSIGN_REF, manu=SAMPLE_MANU,
+                        ref='Jill', extra='Extra!'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jill'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jack'))
+    print(handle_action(SUBMITTED, WITHDRAW, manu=SAMPLE_MANU))
+    print(handle_action(SUBMITTED, REJECT, manu=SAMPLE_MANU))
 
 
 if __name__ == '__main__':
