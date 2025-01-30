0680a9615a5fc6d0ab9940bdee43a086e01991b4
Melanie <ma6700@nyu.edu>
2025-01-30 12:26:44 -0500

    update formatting of wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,23 @@
+# Import the application from your project
+# Make sure this import is correct
+from server.endpoints import app as application
 import sys
 import os
 
 # Set the project directory
-project_home = '/home/swetasticfour/SWEtastic-journal'  # Update this to your app's path
+# Update this to your app's path
+project_home = '/home/swetasticfour/SWEtastic-journal'
 if project_home not in sys.path:
     sys.path.append(project_home)
 
 # Activate the virtual environment
-activate_this = '/home/swetasticfour/.virtualenvs/myvirtualenv/bin/activate_this.py'
+activate_this = ("/home/swetasticfour/.virtualenvs/" +
+                 "myvirtualenv/bin/activate_this.py")
 exec(open(activate_this).read(), {'__file__': activate_this})
 
 # Set environment variables
-os.environ['YOUR_PASSWORD_VARIABLE'] = 'swe2024to25'  # Replace with your actual MongoDB password
+# Replace with your actual MongoDB password
+os.environ['YOUR_PASSWORD_VARIABLE'] = 'swe2024to25'
 os.environ['CLOUD_MONGO'] = '1'
-
-# Import the application from your project
-from server.endpoints import app as application  # Make sure this import is correct
 
 application
