b783c8cf1c3456a2275b1b1e46282059ece053c4
Melanie <ma6700@nyu.edu>
2025-01-30 13:10:25 -0500

    removing echo of pythonanwhere password and username

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,10 @@ echo "Virtual env = $VENV"
 
 if [ -z "$DEMO_PA_PWD" ]
 then
-    echo "The PythonAnywhere password var (DEMO_PA_PWD) must be set in the env."
+    echo "The PythonAnywhere password var must be set in the env, export CLOUD_PW=###"
     exit 1
 fi
 
-
-echo "PA user = $PA_USER"
-echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
