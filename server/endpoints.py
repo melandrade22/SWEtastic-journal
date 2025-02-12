36bb5a0aa287abe2a8ca38652894b535476fb441
Meghan Bhatia <meghan.bhatia@nyu.edu>
2025-02-11 23:52:39 -0500

    Added endpoint to recieve manuscript action to endpoint.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -333,3 +333,31 @@ MANU_ACTION_FLDS = api.model('ManuscriptAction', {
     manu.ACTION: fields.String,
     manu.REFEREE: fields.String,
 })
+
+
+@api.route(f'{MANU_EP}/receive_action')
+class ReceiveAction(Resource):
+    """
+    Receive an action for a manuscript.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'Not acceptable')
+    @api.expect(MANU_ACTION_FLDS)
+    def put(self):
+        """
+        Receive an action for a manuscript.
+        """
+        try:
+            print(request. json)
+            manu_id = request. json.get(manu.MANU_ID)
+            curr_state = request.json.get(manu.CURR_STATE)
+            action = request. json.get(manu.ACTION)
+            kwargs = t
+            kwargs [manu. REFEREE] = request.json.get(manu.REFEREE)
+            ret = manu.handle_action(manu_id, curr_state, action, **kwargs)
+        except Exception as err:
+            raise wz. NotAcceptable(f'Bad action: ' f'{err=}')
+        return {
+            MESSAGE: 'Action received!',
+            RETURN: ret,
+        }
